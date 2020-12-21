fab70d56e7d43714fe3756ec2041940adad7c46f
Erin Mahoney <emahoney@indeed.com>
2020-12-21 11:56:11 -0600

    Remove unused dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,14 +72,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: 'gatsby-plugin-social-cards',
-      options: {
-        authorImage: "./content/assets/profile-pic.jpg",
-        backgroundImage: "./content/assets/template.jpg",
-        defaultAuthor: "Erin Mahoney",
-        design: "default",
-      }
-    }
   ],
 }
