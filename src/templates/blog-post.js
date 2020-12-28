670c158203c602d87e27c52944edc96e11102943
Erin Mahoney <emahoney@indeed.com>
2020-12-28 14:10:48 -0600

    Allow post content to render

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,7 +26,7 @@ const BlogPostTemplate = ({ data, location }) => {
           <p>{post.frontmatter.date}</p>
         </header>
         <section
-          dangerouslySetInnerHTML={{ __html: post.description }}
+          dangerouslySetInnerHTML={{ __html: post.html }}
           itemProp="articleBody"
         />
       </article>
